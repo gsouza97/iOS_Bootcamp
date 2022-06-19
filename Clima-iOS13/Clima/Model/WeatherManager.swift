67effe0e8b1691109216fdc5c22570154e0740c4
gsouza97 <gabrielss_@outlook.com>
2022-06-19 12:02:49 -0300

    Clima JSON Decoding and Formatting

--- Clima-iOS13/Clima/Model/WeatherManager.swift
+++ Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -23,9 +23,6 @@ struct WeatherManager {
             let session = URLSession(configuration: .default)
             
             //3. Give the session a task
-            
-            
-            
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
                     print(error!)
@@ -47,13 +44,19 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.name)
-            print(decodedData.main.temp)
-            print(decodedData.weather[0].description)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.temperatureString)
         } catch {
             print(error)
         }
         
     }
+    
+    
     
 }
