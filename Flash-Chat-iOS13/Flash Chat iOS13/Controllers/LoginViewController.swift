fe51011a590b045237135bc8c99c59638d9f874e
gsouza97 <gabrielss_@outlook.com>
2022-06-27 18:39:23 -0300

    User Login

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -15,6 +16,27 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                    self.showAlertError(error: e)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
+        
+    }
+    
+    func showAlertError(error: Error) {
+        let alert = UIAlertController(title: "An error has ocurred", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: { action in
+            print("Tapped Dismiss")
+        }))
+        
+        present(alert, animated: true)
+                        
     }
     
 }
