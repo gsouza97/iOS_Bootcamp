b6b89df8fc4a45d3953eb83c72110e2203974030
gsouza97 <gabrielss_@outlook.com>
2022-06-27 19:09:23 -0300

    User Logout

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
+        
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-
+    @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        
+        do {
+            try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+            showAlertError(error: signOutError)
+        }
+        
+    }
+    
+    func showAlertError(error: Error) {
+        let alert = UIAlertController(title: "An error has ocurred", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: { action in
+            print("Tapped Dismiss")
+        }))
+        
+        present(alert, animated: true)
+                        
+    }
+    
 }
