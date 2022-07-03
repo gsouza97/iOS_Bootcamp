3f3587bd629f4f6a7f66720efda4f4ce89fb17cb
gsouza97 <gabrielss_@outlook.com>
2022-07-03 17:37:51 -0300

    TableView and Custom Cells using .xib

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,12 +14,21 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's Up?"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
         self.title = K.appName
         navigationItem.hidesBackButton = true
         
+        // Linkando o arquivo .xib
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -46,6 +55,22 @@ class ChatViewController: UIViewController {
         
         present(alert, animated: true)
                         
+    }
+    
+}
+
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
+        cell.label.text = messages[indexPath.row].body
+        
+        return cell
     }
     
 }
